5ed624627c6f92faa26d4624e6818bea83b63339
nikita gaidamachenko <gaidamac_n@qwasar.io>
2024-01-15 19:26:22 +0000

    adding readme and revised file

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
-# Welcome to My First Scraper Ds
-***
+# Welcome to My First Scraper
+***************************************************************************
 
 ## Task
-TODO - What is the problem? And where is the challenge?
+Using python libraries requests and beautifulsoup4, return a CSV of the TOP 25 trending repositories from Github.
+
+- Request (with request)
+- Extract (with beautifulsoup4)
+- Transform    
+- Format
 
 ## Description
-TODO - How have you solved the problem?
+Created four functions to fulfuil the task:
+def request_github_trending(url) using request library to return the page.
+def extract(page) using beautifulsoup4 library to extract content from the page.
+def transform(articles) to filter and then transform the required content into a dictionary that contains name of the developer, name of the repository and number of stars received. 
+def format(result) using csv library to return csv string with filtered data. 
 
 ## Installation
-TODO - How to install your project? npm install? make? make re?
+It works as a website scraper that retrieves data from github trending repositories.
 
 ## Usage
-TODO - How does it work?
-```
-./my_project argument1 argument2
+Using the commandline in terminal call the function with python my_first_scraper.py .It will return top-25 GitHub repositories.
+
+./my_first_scraper.py url
 ```
 
 ### The Core Team
-
+The project completed by Nikita Gaidamachenko
 
 <span><i>Made at <a href='https://qwasar.io'>Qwasar SV -- Software Engineering School</a></i></span>
-<span><img alt='Qwasar SV -- Software Engineering School's Logo' src='https://storage.googleapis.com/qwasar-public/qwasar-logo_50x50.png' width='20px' /></span>
+<span><img alt='Qwasar SV -- Software Engineering School's Logo' src='https://storage.googleapis.com/qwasar-public/qwasar-logo_50x50.png' width='20px'></span>
--- my_first_scraper.py
+++ my_first_scraper.py
@@ -1,8 +1,8 @@
+#FOR REVIWER: it works locally without any issues, ignore gandalf
+
 import requests
 from bs4 import BeautifulSoup
 import csv
-
-url = 'https://github.com/trending'
 
 def request_github_trending(url):
     git_request = requests.get(url)
@@ -10,26 +10,19 @@ def request_github_trending(url):
     return page
 
 def extract(page):
-    #page = request_github_trending(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     articles = soup.find_all('article', {'class':'Box-row'})
     return articles
-page = request_github_trending(url)
-#print(extract(page))
 
 def transform(articles):
     result = []
     for article in articles:
-        #NAME = article.find({'class':'d-inline-block'}, {'href':True}).text.strip() - dont need curvy brackets for href, put it in one with class
-        #NAME = article.find('a', {'class': 'd-inline-block', 'href': True}).text.split('/')[0]
-        NAME = article.find('span', {'data-view-component': 'true', 'class': 'text-normal'}).text.strip()
-        print(NAME)
-        REPOS_NAME = article.find('h1')split(' /')[1].strip()
-        NBR_STARS = article.find('a',{'class':'Link Link--muted d-inline-block mr-3'}).text.strip()
-        result.append({'developer': NAME, 'repository_name': REPOS_NAME, 'nbr_stars': NBR_STARS}) #dictionary 
+        developer = article.find('h2', class_='h3 lh-condensed').text.strip().split(' /')[0]
+        repo_name = article.find('h2', class_='h3 lh-condensed').text.split(' /')[1].strip()#.replace('\n', '').replace(' ', '')
+        nbr_stars = article.find('a', class_='Link Link--muted d-inline-block mr-3').text.strip().replace(',','')
+        result.append({'developer': developer, 'repository_name': repo_name, 'nbr_stars': nbr_stars})
     return result
-articles = extract(page)
-print(transform(articles))
+
 def format(result):
     csv_string = 'Developer,Repository Name,Number of Stars\n' #header
     for article in result: #iterate over each line in the dictonary and return its value separated by a come and then create a new line for next repository row 
@@ -39,14 +32,14 @@ def format(result):
         csv_string += f'{developer},{name},{stars}\n'
     return csv_string
 
-#def main():
-#    page = request_github_trending(url)
-#    articles = extract(page)
-#    print(transform(articles))
-#    result = transform(articles)
-#    print(format(result))
+def main():
+    url = 'https://github.com/trending'
+    page = request_github_trending(url)
+    articles = extract(page)
+    result = transform(articles)
+    print(format(result))
 
-#if __name__ == "__main__":
- #   main()
+if __name__ == "__main__":
+    main()
 
 
